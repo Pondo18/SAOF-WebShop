021cf143030c34ceec53637c67dc29ca417f1cde
Moritz Moser <moritz@baseofm.de>
2021-04-12 13:59:02 +0200

    Erstelle ein Spring Boot Projekt für das Backend

---
+++ src/test/java/de/hdbw/webshop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hdbw.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
