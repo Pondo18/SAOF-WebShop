0fde34a87298fc08fd289407338bfe645195cfe8
Moritz Moser <moritz@baseofm.de>
2021-04-14 16:23:59 +0200

    Zeige unter dem Endpoint "/books" eine Liste aller Bücher aus der DB an [VA-32]

---
+++ src/main/java/de/hdbw/webshop/model/Book.java
@@ -0,0 +1,81 @@
+package de.hdbw.webshop.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String bookName;
+    private String author;
+    private String publisher;
+    private LocalDate publishingDate;
+
+
+    public Book(String bookName, String author, String publisher, LocalDate publishingDate) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publisher = publisher;
+        this.publishingDate = publishingDate;
+    }
+
+    public Book() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public LocalDate getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(LocalDate publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", publishingDate=" + publishingDate +
+                '}';
+    }
+}
--- src/main/java/de/hdbw/webshop/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package de.hdbw.webshop.repository;
-
-import de.hdbw.webshop.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-
-}
