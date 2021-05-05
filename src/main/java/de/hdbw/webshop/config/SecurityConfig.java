a8cb6b211d4a5287f6f150142c615540314ac72d
listentoleo <leoforinformation@gmail.com>
2021-05-05 16:27:47 +0200

    Füge funktioniernde Login Page und zugehöriges Styling hinzu [VA-63]

--- src/main/java/de/hdbw/webshop/config/SecurityConfig.java
+++ src/main/java/de/hdbw/webshop/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
+                .loginPage("/login.html")
                 .defaultSuccessUrl("/artworks");
     }
 
