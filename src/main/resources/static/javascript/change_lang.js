9f0cdcf6f7a138b323931e4333242336892a1794
Moritz Moser <moritz@baseofm.de>
2021-06-27 18:48:31 +0200

    Fixe die Redirection nach Sprache ändern[VA-127]

--- src/main/resources/static/javascript/change_lang.js
+++ src/main/resources/static/javascript/change_lang.js
@@ -1,7 +1,15 @@
 function change_lang() {
     const input_switch = document.getElementById("lang");
-    let url="";
-    const referer = document.referrer;
+    let url;
+    let referer = document.referrer;
+    if (referer === window.location.origin + "/login" || referer === window.location.origin + "/registration/user") {
+        referer = window.location.origin;
+    }
+    try {
+        referer = referer.replace('?lang=de', '');
+        referer = referer.replace('?lang=en', '');
+    } catch(ex) {
+    }
     if (input_switch.checked) {
         url = referer +`?lang=en`;
     } else {
